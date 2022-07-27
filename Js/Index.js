a0bae906fd2fd9b231c4cb953a85db93bdff088f
Diego A Bueno <104845463+DevDbueno@users.noreply.github.com>
2022-07-26 20:02:36 -0500

    Avances a clase 16 POO
    Se realizaron pruebas y cambios a las carpetas en general hasta la numero 26

---
+++ Js/Index.js
@@ -0,0 +1,4 @@
+var car = new Car("AWS199",new Account("Andres Bueno","123456"))
+car.passenger = 4;
+car.printDataCar();
+
---
+++ Python/main.py
@@ -0,0 +1,7 @@
+from car import Car
+from account import Account
+car = Car("UBK29D", Account("Diego Bueno","1095941803"))
+print(vars(car))
+print(vars(car.driver))
+
+
